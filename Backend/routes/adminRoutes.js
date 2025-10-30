e3e8812c0c6512b5cb988308b3f3224c0655282f
Sudhanshu Barnwal <83909429+Sudhanshu3314@users.noreply.github.com>
2025-10-30 12:17:02 +0530

    Update

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
 const { getDinnerReport } = require("../Controllers/DinnerReportController");
+const { getLunchReport } = require("../Controllers/LunchReportController")
 
 // Get all users (for Admin Dashboard)
 router.get("/users", async (req, res) => {
@@ -15,6 +16,7 @@ router.get("/users", async (req, res) => {
 });
 
 // 🍽️ Dinner report (only for admins, after 4:05 PM)
-router.get("/dinner-report",getDinnerReport);
+router.get("/dinner-report", getDinnerReport);
+router.get("/lunch-report", getLunchReport);
 
 module.exports = router;
