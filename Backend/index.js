3d94b57524fdb8973fd33cf459886cbb1711d2f4
Sudhanshu Barnwal <83909429+Sudhanshu3314@users.noreply.github.com>
2025-12-24 11:48:40 +0530

    Update

--- Backend/index.js
+++ Backend/index.js
@@ -30,27 +30,27 @@ const adminRouter = require("./routes/adminRoutes");
 const uploadRouter = require("./routes/uploadRoutes");
 const menuRouter = require("./routes/menuRoutes");
 
-// app.use(cors({
-//     origin: true,
-//     credentials: true
-// }));
-
-const allowedOrigins = ["https://igidr-cmms.netlify.app", "http://localhost:1234", "https://igidr-cmma.netlify.app"]; // Add your Netlify URL here
-
 app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed HTTP methods
-    credentials: true, // If you need to send cookies/authorization headers
-    optionsSuccessStatus: 204 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+    origin: true,
+    credentials: true
 }));
+
+// const allowedOrigins = ["https://igidr-cmms.netlify.app", "http://localhost:1234", "https://igidr-cmma.netlify.app"]; // Add your Netlify URL here
+
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         // allow requests with no origin (like mobile apps or curl requests)
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) {
+//             const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     },
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed HTTP methods
+//     credentials: true, // If you need to send cookies/authorization headers
+//     optionsSuccessStatus: 204 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+// }));
 
 const PORT = process.env.PORT || 8080;
 
